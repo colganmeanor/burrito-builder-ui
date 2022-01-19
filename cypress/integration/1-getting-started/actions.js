37a2db5353617425375e72a806bcedccbf516481
Colgan Meanor <colganmeanor7@gmail.com>
2022-01-19 11:27:10 -0600

    Add cypress npm dependency

---
+++ cypress/integration/1-getting-started/actions.js
@@ -0,0 +1,16 @@
+describe('burrito app user actions', () => {
+    beforeEach(() => {
+  
+  
+      cy.visit('http://localhost:3000/')
+    })
+  
+    it('displays two todo items by default', () => {
+     
+    })
+  
+    it('can add new todo items', () => {
+  
+  })
+  
+  })
---
+++ cypress/integration/1-getting-started/dom.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+
+
+describe('burrito app DOM', () => {
+  beforeEach(() => {
+
+
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('displays two todo items by default', () => {
+   
+  })
+
+  it('can add new todo items', () => {
+
+})
+
+})
